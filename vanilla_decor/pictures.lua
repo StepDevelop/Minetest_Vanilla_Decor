c6cae60a144f3a5110c39a9674311e8f6b2d4df8
Stefano Peris <stefanop.dev@gmail.com>
2016-05-15 14:06:07 +0200

    Clean project, fixing, add description, screenshot)

---
+++ vanilla_decor/pictures.lua
@@ -0,0 +1,48 @@
+--[[
+
+ Copyright (C) 2016 Stefano <StepDevelop> Peris <stefanop.dev@gmail.com>
+ 
+ vanilla_decor is free software: you can redistribute it and/or modify it
+ under the terms of the GNU General Public License as published by the
+ Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ vanilla_decor is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License along
+ with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+--]]
+
+
+minetest.register_node("vanilla_decor:picture_wooden_planks", {
+	description = "Picture 1",
+	drawtype = "nodebox",
+	tiles = {"stepdevelop_avatar.png"},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	sunlight_propagates = true,
+	buildable_to = false,
+	walkable = true,
+	climbable = false,
+	groups = {cracky=3},
+	stack_max = 250,
+	sounds = default.node_sound_stone_defaults(),
+		node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, 0.4375, 0.5, 0.5, 0.5}, -- NodeBox1
+		}
+	}
+})
+
+
+-- Quadro doppio
+
+-- {-0.5, -0.5, 0.4375, 1.5, 0.5, 0.5}, -- NodeBox1
+
+
+
